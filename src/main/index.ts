e8559d013490a5a5ccfaa4cc7da90309f83ea464
andrey <xdeleted.al@gmail.com>
2026-08-05 18:10:42 +0200

    fix(main): sentry initializing

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,10 +1,3 @@
-import 'source-map-support/register'
-import packageJson from '../../package.json'
-import banner from './banner.txt?raw'
-import { initSentry } from './utils/sentry'
-
-console.log(`\n${banner}\n Version: ${packageJson.version}\n`)
-initSentry()
-
+import './init'
 import { App } from './app/App'
 new App().boot()
---
+++ src/main/init.ts
@@ -0,0 +1,7 @@
+import 'source-map-support/register'
+import packageJson from '../../package.json'
+import banner from './banner.txt?raw'
+import { initSentry } from './utils/sentry'
+
+console.log(`\n${banner}\n Version: ${packageJson.version}\n`)
+initSentry()
